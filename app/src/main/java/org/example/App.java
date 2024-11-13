a38044f3eb2c559c999bfed99d42be2e5011c4a2
Dinuka Pramod <dinukah98@gmail.com>
2024-11-13 17:01:00 +0530

    Main.java started

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,31 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+
+    // MAIN METHOD
+    public static void main(String[] args) {
+
+        start();
+
+        welcome("Programmer");
+
+        System.out.println(getGreeting("Developer"));
+
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    // FIRST METHOD (SHOW APP HAS STARTED)
+    public static void start() {
+        System.out.println("Application Has Started");
     }
+
+    // PASSING A VALUE
+    public static void welcome(String name) {
+        System.out.println("Welcome " + name);
+    }
+
+    // SHOWCASE THE USE OF RETURN
+    public static String getGreeting(String name) {
+        return "Hello " + name;
+    }
+
 }
